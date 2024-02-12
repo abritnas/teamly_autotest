777267e95ce3f8fd83c817ea7d7080b0ec4dd0af
a.evlanova <arina.evlanova68@gmail.com>
2024-02-12 11:33:32 +0300

    changes in create_driver.py

--- authorization/create_driver.py
+++ authorization/create_driver.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 class Browser:
     browser = None
 
+    # jghdjghd
     def create_browser(self, link):
         self.browser = webdriver.Chrome()
         self.browser.get(link)
