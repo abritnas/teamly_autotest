530b992c78911dc4782f008d48a8e3ba5201f2fc
a.evlanova_mac <arina.evlanova68@gmail.com>
2024-02-19 14:12:38 +0300

    изменения в spaces

--- spaces/test_main_spaces.py
+++ spaces/test_main_spaces.py
@@ -1,15 +1,24 @@
-
-
 from test_list_of_spaces import TestSpaces
-from authorization import test_authorization
-import sys
 import os
+import sys
 
-sys.path.insert(1, os.path.join(sys.path[0], '../../../authorization'))
-from test_main_authorization import test_authorization_correct_username_correct_password
-from create_driver import Browser
+sys.path.append(os.path.abspath('../authorization'))
+import test_main_authorization
+# from data import Config
+
+# import create_driver
+
+# sys.path.insert(1, '../../../authorization')
+# import authorization.test_main_authorization
+# from authorization.create_driver import Browser
+
+data = Config()
+settings = data.read_config('config.ini')
+config_username = settings["Authorization"]["username_correct"]
+config_password = settings["Authorization"]["password_correct"]
+
 
 
 def test_open_page():
     run = TestSpaces()
-    test_authorization_correct_username_correct_password()
+    test_main_authorization.test_authorization_correct_username_correct_password(config_username, config_password)
